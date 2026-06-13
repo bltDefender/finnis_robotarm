b9bcd5d1fbda105b9ab4f896a33f386f16eb3441
Mirco <37219611+bltDefender@users.noreply.github.com>
2026-06-13 17:58:05 +0200

    Add config.h for WiFi and MQTT secrets

---
+++ src/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// =====================================================
+// Secrets / lokale Konfiguration
+// Diese Datei nicht ins öffentliche Repo committen.
+// =====================================================
+
+// WLAN
+static const char* WIFI_SSID = "";
+static const char* WIFI_PASS = "";
+
+// MQTT
+static const char* MQTT_HOST = "192.168.188.220";
+static const uint16_t MQTT_PORT = 1883;
+static const char* MQTT_USER = "";
+static const char* MQTT_PASS = "";
+static const char* MQTT_CLIENT_ID = "finnis_robotarm_esp32";
